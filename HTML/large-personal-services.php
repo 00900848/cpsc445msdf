99aa36c0855620706b655cf92d81af772f481122
00900848 <mattsc95@protonmail.com>
2019-04-10 20:37:38 -0400

    Added more content to results

--- HTML/large-personal-services.php
+++ HTML/large-personal-services.php
@@ -57,7 +57,7 @@ include('dbconnect.php');
 		</div>
 		<div class="option">
 			<?php 
-				$query = "SELECT * FROM WebServices where w_id=06";
+				$query = "SELECT * FROM WebServices where w_id=26";
 				$result = $conn->query($query);
 				if($row=$result->fetch_assoc()) {
 					echo "<h3>Option 3</h3>";
